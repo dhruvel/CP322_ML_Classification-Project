184cf0dd78b33afc0117da5c4e6e9d3fac0fae16
pabloj2001 <pabloj2001@gmail.com>
2023-10-11 17:53:35 -0400

    Moved models folder

--- src/utils.py
+++ src/utils.py
@@ -10,7 +10,7 @@ def evaluate_acc(predictions, labels):
     return sum(predictions == labels) / len(labels)
 
 def save_model(args, model: ModelInterface, filename: str):
-    with open("models/" + filename, "a") as f:
+    with open("../models/" + filename, "a") as f:
         for arg in args:
             f.write(str(arg) + ",")
         for param in model.params:
@@ -18,7 +18,7 @@ def save_model(args, model: ModelInterface, filename: str):
         f.write(str(model.b[0]) + "\n")
 
 def load_models(filename: str, arg_num: int):
-    with open("models/" + filename, "r") as f:
+    with open("../models/" + filename, "r") as f:
         models = []
         for line in f:
             values = line.split(",")
