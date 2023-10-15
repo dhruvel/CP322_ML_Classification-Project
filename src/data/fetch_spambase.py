88941d786b3f33c056cf50b4a017bbcd79d81e66
pabloj2001 <pabloj2001@gmail.com>
2023-10-15 14:07:36 -0400

    Updated diabetes and spambase loading

--- src/data/fetch_spambase.py
+++ src/data/fetch_spambase.py
@@ -1,14 +1,17 @@
 import numpy as np
 
-
 # fetch dataset
-spambase_data = np.loadtxt("CP322_ML_Classification-Project/data/spambase/spambase.data", delimiter=',')
+spambase_data = np.loadtxt("../data/spambase/spambase.data", delimiter=',')
 arr = np.array(spambase_data)
 
 # remove duplicate rows
 arr = np.unique(arr, axis=0)
 
-# save cleaned dataset
-with open("spambase_data_clean.csv", "w") as f:
-    np.savetxt(f, arr, delimiter=",", fmt="%s")
+# normalize capital_run_length_average, capital_run_length_longest, capital_run_length_total columns
+arr[:, 54] = (arr[:, 54] - np.mean(arr[:, 54])) / np.std(arr[:, 54])
+arr[:, 55] = (arr[:, 55] - np.mean(arr[:, 55])) / np.std(arr[:, 55])
+arr[:, 56] = (arr[:, 56] - np.mean(arr[:, 56])) / np.std(arr[:, 56])
 
+# save cleaned dataset
+# with open("spambase_data_clean.csv", "w") as f:
+#     np.savetxt(f, arr, delimiter=",", fmt="%s")
