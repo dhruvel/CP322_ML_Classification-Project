403010d532f779107b2c11c0011ea4dbadfe9dc2
Pablo Araujo Camacho <pabloj2001@gmail.com>
2023-12-04 17:39:57 -0500

    Init Proj 3

---
+++ Project3/src/cnn.py
@@ -0,0 +1,1 @@
+from data import trainloader, testloader, classes
---
+++ Project3/src/data.py
@@ -0,0 +1,27 @@
+import os
+import torch
+import torchvision
+import torchvision.transforms as transforms
+
+# Ensure we're running from src directory
+os.chdir(os.path.dirname(os.path.realpath(__file__)))
+
+# Normalize the data
+_transform = transforms.Compose([
+    transforms.ToTensor(),
+    transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+])
+
+# PyTorch recommends a batch size of 4, but I feel that's too small
+# Increasing batch size reduces training time, but might also reduce accuracy
+batch_size = 32
+
+# Load train data
+_trainset = torchvision.datasets.CIFAR10(root='../data', train=True, download=True, transform=_transform)
+trainloader = torch.utils.data.DataLoader(_trainset, batch_size=batch_size, shuffle=True, num_workers=4)
+
+# Load test data
+_testset = torchvision.datasets.CIFAR10(root='../data', train=False, download=True, transform=_transform)
+testloader = torch.utils.data.DataLoader(_testset, batch_size=batch_size, shuffle=False, num_workers=4)
+
+classes = ('plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
